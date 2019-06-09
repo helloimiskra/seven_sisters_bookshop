0d44b57f85589fe9d8c9cedd8c60bfd65d91f2f1
Iskra B <ibatistapoblete@gmail.com>
2019-06-09 13:10:18 +0000

    tests edge cases for book instance properties

--- lib/seven_sisters_bookshop/school.rb
+++ lib/seven_sisters_bookshop/school.rb
@@ -61,6 +61,7 @@ class SevenSistersBookshop::School
 
   def self.scrape_schools
     doc = Nokogiri::HTML(open("https://riverdogbookco.com/sevensistersstories/"))
+    url = Nokogiri::HTML(open("https://shop.riverdogbookco.com/book/9780553375930"))
    binding.pry
     schools = []
     school_names = doc.css("div.fl-rich-text p strong").text.gsub!(".", "")
@@ -68,8 +69,20 @@ class SevenSistersBookshop::School
     schools.flatten!
   end
 
-  def self.scrape_books
-  end
+  # doc.css("div.fl-photo-content.fl-photo-img-jpg").css('alt').text
+
+  #book urls doc.css('div.fl-photo-content.fl-photo-img-jpg a').map{ |link| link['href'].include?('shop')? link['href'] : nil }.compact
+
+  #book author url.css("div.abaproduct-authors a").text
+  #book price url.css("div.abaproduct-price").text.strip
+
+  #book desc 1 url.css("div.abaproduct-body").first.text
+  #book desc 2 url.css("div.abaproduct-body b").first.text
+  # def self.scrape_books
+  #   url = Nokogiri::HTML(open(https://shop.riverdogbookco.com/book/9780812983470)
+  # end
+
+
 
 
 end
