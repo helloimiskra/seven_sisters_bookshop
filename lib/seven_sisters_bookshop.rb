6cba5dd7df798cd42d306b9b80c7fd99aaf22ade
Iskra B <ibatistapoblete@gmail.com>
2019-06-09 07:00:08 +0000

    tests first CLI interface

--- lib/seven_sisters_bookshop.rb
+++ lib/seven_sisters_bookshop.rb
@@ -1,6 +1,8 @@
-require "seven_sisters_bookshop/version"
+require_relative "./seven_sisters_bookshop/version"
 
-module SevenSistersBookshop
-  class Error < StandardError; end
-  # Your code goes here...
-end
+# module SevenSistersBookshop
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+
+require_relative './seven_sisters_bookshop/cli'
---
+++ lib/seven_sisters_bookshop/cli.rb
@@ -0,0 +1,7 @@
+class SevenSistersBookshop::CLI
+
+  def call
+    puts "\n\nWelcome to the Seven Sisters' Bookshop!\n\nPlease choose a college: \n\n"
+  end
+
+end
