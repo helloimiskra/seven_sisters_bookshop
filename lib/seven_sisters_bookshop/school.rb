a274cca7c1f36cc9adac3327e142d795752275e7
Iskra B <ibatistapoblete@gmail.com>
2019-06-13 12:52:09 +0000

    updates school class

--- lib/seven_sisters_bookshop/school.rb
+++ lib/seven_sisters_bookshop/school.rb
@@ -19,15 +19,85 @@ class SevenSistersBookshop::School
     self.class.all << self
   end
   
-  def self.get_school_name_from_text(text)
-      return :barnard if text.include?("barnard")
-      return :brynmawr if text.include?("brynmawr")
-      return :mountholyoke if text.include?("mount")
-      return :radcliffe if text.include?("radcliffe")
-      return :smith if text.include?("smith")
-      return :vassar if text.include?("vassar")
-      return :wellesley if text.include?("wellesley")
+  def self.scrape_schools
+    doc = Nokogiri::HTML(open("https://riverdogbookco.com/sevensistersstories/"))
+    schools = []
+    school_names = doc.css("div.fl-rich-text p strong").text.gsub!(".", "")
+    schools << school_names.gsub!("and ", "").split(", ")
+    schools.flatten!
+    schools
   end
+  
+  def self.scrape_books
+    doc = Nokogiri::HTML(open("https://riverdogbookco.com/sevensistersstories/"))
+    booklist = doc.css('img').map { |l| l.attr('alt') }
+    booklist.delete_if{|x| x == ""}
+  end
+  
+  def self.get_school_name_from_text(text)
+    return :barnard if text.include? "Barnard"
+    return :brynmawr if text.include? "Bryn"
+    return :mountholyoke if text.include? "mount-holyoke-college-2-logo-png-transparent"
+    return :radcliffe if text.include? "Radcliffe"
+    return :smith if text.include? "Smith"
+    return :vassar if text.include? "Vassar"
+    return :wellesley if text.include? "Wellesley"
+  end
+  
+  def self.sort_books_into_schools
+
+    colleges = [
+        "Barnard College logo",
+        "Bryn Mawr College logo",
+        "mount-holyoke-college-2-logo-png-transparent",
+        "Radcliffe logo",
+        "Smith College logo",
+        "Vassar logo",
+        "Wellesley logo"
+        ]
+
+    book_types = [
+        "Audio Books Link",
+        "e-books link"
+        ]
+
+    item_array = self.scrape_books
+
+    college_books = {
+        barnard: [],
+        brynmawr: [],
+        mountholyoke: [],
+        radcliffe: [],
+        smith: [],
+        vassar: [],
+        wellesley: []
+      }
+
+    current_school = nil
+    is_paperback = true
+  
+    item_array.map do |item|
+      if colleges.include?(item)
+        current_school = self.get_school_name_from_text(item)
+        is_paperback = true
+      end
+    end
+    
+    item_array.map do |item|
+      if colleges.include?(item)
+        current_school = self.get_school_name_from_text(item)
+        is_paperback = true
+      elsif book_types.include? item
+        is_paperback = false
+      elsif item == "paper books link"
+        is_paperback = true
+      else
+        college_books[current_school] << item if is_paperback
+      end
+    end
+    college_books
+  end
+  
 
 
 end
